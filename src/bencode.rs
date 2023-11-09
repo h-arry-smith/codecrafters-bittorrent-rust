456082631c87b65901753f9d5fd3c1e1700ea723
Harry Smith <harry@hsmith.net>
2023-11-09 15:34:02 +0800

    Stage Two

--- src/bencode.rs
+++ src/bencode.rs
@@ -13,13 +13,14 @@ impl<'a> Bencode<'a> {
 
     pub fn decode(&mut self) -> serde_json::Value {
         match self.peek() {
+            Some('i') => self.decode_integer(),
             Some(_) => self.decode_string(),
             None => panic!("Unexpected end of input"),
         }
     }
 
     fn decode_string(&mut self) -> serde_json::Value {
-        let string_length = self.decode_number();
+        let string_length = self.decode_integer_number();
         self.consume(':').expect("Expected ':' after string length");
 
         let string_slice = &self.string[self.position..self.position + string_length as usize];
@@ -27,11 +28,19 @@ impl<'a> Bencode<'a> {
         serde_json::Value::String(string_slice.to_string())
     }
 
-    fn decode_number(&mut self) -> i64 {
+    fn decode_integer(&mut self) -> serde_json::Value {
+        self.consume('i').expect("Expected 'i' at start of integer");
+        let integer = self.decode_integer_number();
+        self.consume('e').expect("Expected 'e' at end of integer");
+
+        serde_json::Value::Number(integer.into())
+    }
+
+    fn decode_integer_number(&mut self) -> i64 {
         let mut number_string = String::new();
         loop {
             match self.peek() {
-                Some(c) if c.is_ascii_digit() => {
+                Some(c) if c.is_ascii_digit() || c == '-' => {
                     number_string.push(c);
                     self.next();
                 }
@@ -82,5 +91,19 @@ mod tests {
             decoded_value,
             serde_json::Value::String("hello world".to_string())
         );
+    }
+
+    #[test]
+    fn positive_integer() {
+        let mut bencode = super::Bencode::new("i123e");
+        let decoded_value = bencode.decode();
+        assert_eq!(decoded_value, serde_json::Value::Number(123.into()));
+    }
+
+    #[test]
+    fn negative_integer() {
+        let mut bencode = super::Bencode::new("i-123e");
+        let decoded_value = bencode.decode();
+        assert_eq!(decoded_value, serde_json::Value::Number((-123).into()));
     }
 }
